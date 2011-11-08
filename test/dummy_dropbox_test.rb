2995a1e62522e8b90c66a040719c5bddc8d1e087
Artur Szwed <artur1979@googlemail.com>
2011-11-08 18:04:15 +0100

    Added move, rename functionalities and some smaller fixes for list and metadata

--- test/dummy_dropbox_test.rb
+++ test/dummy_dropbox_test.rb
@@ -12,6 +12,7 @@ class DummyDropboxTest < Test::Unit::TestCase
   
   def test_download
     assert_equal( "File 1", @session.download( '/file1.txt' ) )
+    assert_raise(Dropbox::UnsuccessfulResponseError) {@session.download( '/filex.txt')}
   end
   
   def test_metadata
@@ -20,8 +21,8 @@ class DummyDropboxTest < Test::Unit::TestCase
   end
   
   def test_list
-    assert_equal(['/file1.txt', '/folder1'], @session.list('').map{ |e| e.path } )
-    assert_equal(['folder1/file2.txt', 'folder1/file3.txt'], @session.list('folder1').map{ |e| e.path } )
+    assert_equal(['/file1.txt', '/folder1'].sort, @session.list('').map{ |e| e.path }.sort)
+    assert_equal(['folder1/file2.txt', 'folder1/file3.txt'].sort, @session.list('folder1').map{ |e| e.path }.sort )
   end
 
   def test_delete
@@ -38,12 +39,37 @@ class DummyDropboxTest < Test::Unit::TestCase
     assert( !File.exists?( "#{DummyDropbox.root_path}/tmp_folder" ) )
   end
   
+  def test_rename
+    FileUtils.mkdir_p( "#{DummyDropbox.root_path}/tmp_folder" )
+    assert( File.exists?( "#{DummyDropbox.root_path}/tmp_folder" ) )
+
+    @session.rename '/tmp_folder', 'temp_folder'
+    assert !@session.list('/').detect {|e| e.path == '/tmp_folder'}
+    assert @session.list('/').detect {|e| e.path == '/temp_folder'}
+    
+    FileUtils.rmdir( "#{DummyDropbox.root_path}/temp_folder" )
+  end
+
+  def test_move
+    FileUtils.mkdir_p( "#{DummyDropbox.root_path}/tmp_folder" )
+
+    @session.move 'file1.txt', 'tmp_folder/file1.txt'
+    assert !@session.list('/').detect {|e| e.path == '/file1.txt'}
+    assert @session.list('/tmp_folder').detect {|e| e.path == '/tmp_folder/file1.txt'}
+    
+    @session.move '/tmp_folder/file1.txt', '/file1.txt'
+
+    FileUtils.rmdir( "#{DummyDropbox.root_path}/tmp_folder" )
+  end
+  
   def test_create_folder
     FileUtils.rm_r( "#{DummyDropbox.root_path}/tmp_folder" )  if File.exists?( "#{DummyDropbox.root_path}/tmp_folder" )
     metadata = @session.create_folder '/tmp_folder'
     assert( File.directory?( "#{DummyDropbox.root_path}/tmp_folder" ) )
     assert( metadata.directory? )
-    
+    assert( metadata.is_dir )
+    assert_equal metadata.revision, 32
+    assert_raise(Dropbox::FileExistsError) {@session.create_folder('/tmp_folder')}
     FileUtils.rm_r( "#{DummyDropbox.root_path}/tmp_folder" )
   end
   
@@ -55,10 +81,9 @@ class DummyDropboxTest < Test::Unit::TestCase
       File.read( "#{DummyDropbox.root_path}/file.txt" )
     )
     assert( !metadata.directory? )
+    assert( !metadata.is_dir )
+    assert_equal metadata.mime_type, 'image/jpeg'
+    assert_equal metadata.revision, 79
     FileUtils.rm_r( "#{DummyDropbox.root_path}/file.txt" )
-    
-    metadata = @session.upload( StringIO.new("stuff"), '/', :as => "stringio.txt")
-    assert( File.exists?( "#{DummyDropbox.root_path}/#{metadata.path}" ) )
-    FileUtils.rm_r( "#{DummyDropbox.root_path}/stringio.txt" ) if File.exists?( "#{DummyDropbox.root_path}/stringio.txt" )
   end
 end
